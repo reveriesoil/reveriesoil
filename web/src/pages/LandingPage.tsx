c141846d65e4136eb2602bdba09417a973d70ff2
ReverieSoil <reveriesoil@github.com>
2026-05-09 01:36:01 +0800

    fix(ui): 更新程序内版本号显示为v0.5.0

--- web/src/pages/LandingPage.tsx
+++ web/src/pages/LandingPage.tsx
@@ -193,7 +193,7 @@ export default function LandingPage() {
         ))}
       </motion.div>
 
-      <div className="landing-footer-left">ReverieSoil 梦壤 OSS 0.35.1</div>
+      <div className="landing-footer-left">ReverieSoil 梦壤 OSS 0.5.0</div>
       <div className="landing-footer-right">开源版 · WeiCui / 微萃科技</div>
 
       <SettingsModal open={settingsOpen} onClose={() => setSettingsOpen(false)} />
